0f25cb6c942d5defbc5289b2d5d5b4fab432062c
wty4427300 <wty4427300@gmail.com>
2023-11-03 15:59:37 +0800

    写完第一个方法

--- src/main/java/com/KVServer.java
+++ src/main/java/com/KVServer.java
@@ -1,23 +1,43 @@
 package com;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.locks.Lock;
 
 public class KVServer {
-    private Mutex mutex;
+    private Lock mu;
     /**
      * 存储多个值的多个版本
      */
     private Map<String, Map<Long, Version>> storage;
 
+    public KVServer() {
+        this.mu = new Mutex();
+        this.storage = new HashMap<>();
+    }
+
     public Version getLockedVersion(Paxoskv.PaxosInstanceId id) {
-        this.mutex.lock();
+        this.mu.lock();
         String key = id.getKey();
         long ver = id.getVer();
+        //
         Map<Long, Version> s = this.storage.get(key);
-        if(s!=null && !s.isEmpty()){
-
+        if (Objects.isNull(s)) {
+            s = new HashMap<>();
+            this.storage.put(key, s);
         }
+        Version v = s.get(ver);
+        if (Objects.isNull(v)) {
+            v = new Version();
+            Paxoskv.Acceptor acceptor = Paxoskv.Acceptor.newBuilder().build();
+            v.setAcceptor(acceptor);
+        }
+        v.getMu().lock();
+        return v;
+    }
+
+    public Paxoskv.Acceptor Accept(Paxoskv.Proposer proposer){
         return null;
     }
 }
--- src/main/java/com/Version.java
+++ src/main/java/com/Version.java
@@ -4,6 +4,26 @@ package com;
 import java.util.concurrent.locks.Lock;
 
 public class Version {
-    private Lock lock;
+    private Lock mu;
     private Paxoskv.Acceptor acceptor;
+
+    public Version() {
+        this.mu = new Mutex();
+    }
+
+    public Lock getMu() {
+        return mu;
+    }
+
+    public void setMu(Lock mu) {
+        this.mu = mu;
+    }
+
+    public Paxoskv.Acceptor getAcceptor() {
+        return acceptor;
+    }
+
+    public void setAcceptor(Paxoskv.Acceptor acceptor) {
+        this.acceptor = acceptor;
+    }
 }
